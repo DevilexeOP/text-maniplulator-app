d651bb7a76d49ad06b50c32ff6f8eaae3a0fcfa8
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2022-08-25 07:37:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/Textfrom";
+import Alert from "./components/Alert";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const [redMode, setRedMode] = useState("light");
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#132032";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "Text Manipulator - Dark Mode";
+      // setInterval(() => {
+      //   document.title = "Text Manipulator - Buy Premium";  Grab Users Attention
+      // }, 3000);
+      // setInterval(() => {
+      //   document.title = "Text Manipulator - Install Now";  Grab Users Attention
+      // }, 2000);
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "Text Manipulator - Light Mode";
+    }
+  };
+
+  const toggleRedMode = () => {
+    if (redMode === "light") {
+      setRedMode("dark");
+      document.body.style.backgroundColor = "#B25068";
+      showAlert("Red Dark Mode has been enabled", "danger");
+      document.title = "Text Manipulator - Red Dark Mode";
+    } else {
+      setRedMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "Text Manipulator - Light Mode";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Router> */}
+      <Navbar
+        title="Text Manipulator"
+        mode={mode}
+        redMode={redMode}
+        toggleMode={toggleMode}
+        toggleRedMode={toggleRedMode}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route
+              exact
+              path="/"
+              element={ */}
+        <TextForm
+          heading="Enter Your Text To Analyze"
+          mode={mode}
+          redMode={redMode}
+          showAlert={showAlert}
+        />
+        {/* // } */}
+        {/* //   ></Route> */}
+        {/* // </Routes> */}
+      </div>
+      {/* </Router> */}
+    </>
   );
 }
 
---
+++ src/components/Textfrom.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to Uppercase!", "success");
+  };
+  const handleLowClick = () => {
+    // console.log("Uppercase was clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to Lowercase!", "success");
+  };
+  const handleDeleteClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Deleted !", "success");
+  };
+
+  const handleOnChange = (event) => {
+    // console.log("On Change");
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("Enter Text Here");
+  // text = "qwerty"  // Wrong way
+  // setText("new text"); // Correct way
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#132032" }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#132032",
+            }}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-info mx-2" onClick={handleLowClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-secondary mx-2" onClick={handleDeleteClick}>
+          Clear Text
+        </button>
+      </div>
+      <div
+        className="container my-4"
+        style={{ color: props.mode === "dark" ? "white" : "#132032" }}
+      >
+        <h1>Your Text Summary</h1>
+        <p>
+          <p>
+            {
+              text
+                .trim()
+                .split(" ")
+                .filter(function (element) {
+                  return element !== "";
+                }).length
+            }{" "}
+            words and {text.length} characters{" "}
+          </p>
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Enter Something to preview..."}</p>
+      </div>
+    </>
+  );
+}
